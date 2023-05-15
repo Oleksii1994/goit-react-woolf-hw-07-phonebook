d643d37969bb30fb3da7e08168999be817b4ec32
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-05-16 01:53:14 +0300

    optimized extraReducers

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -1,6 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, postContact, deleteContact } from './operations';
 
+const handlePending = state => {
+  state.isLoading = true;
+};
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
@@ -8,59 +16,27 @@ export const contactsSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  // reducers: {
-  //   addContact: {
-  //     reducer(state, action) {
-  //       state.contactsArr.push(action.payload);
-  //     },
-  //     prepare({ name, phone }) {
-  //       return {
-  //         payload: {
-  //           name,
-  //           phone,
-  //           id: nanoid(),
-  //         },
-  //       };
-  //     },
-  //   },
-  //   deleteContactAction: (state, action) => {
-  //     const index = state.contactsArr.findIndex(
-  //       contact => contact.id === action.payload
-  //     );
-  //     state.contactsArr.splice(index, 1);
-  //   },
-  // },
 
   extraReducers: {
-    [fetchContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.pending]: handlePending,
+    [postContact.pending]: handlePending,
+    [deleteContact.pending]: handlePending,
+    [fetchContacts.rejected]: handleRejected,
+    [postContact.rejected]: handleRejected,
+    [deleteContact.rejected]: handleRejected,
+
     [fetchContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.contactsArr = action.payload;
     },
-    [fetchContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
 
-    [postContact.pending](state) {
-      state.isLoading = true;
-    },
     [postContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.contactsArr.push(action.payload);
     },
-    [postContact.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
 
-    [deleteContact.pending](state) {
-      state.isLoading = true;
-    },
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
@@ -69,14 +45,9 @@ export const contactsSlice = createSlice({
       );
       state.contactsArr.splice(index, 1);
     },
-    [deleteContact.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
   },
 });
 
-// export const { addContact, deleteContactAction } = contactsSlice.actions;
 export const { fetchingInProgress, fetchingSuccess, fetchingError } =
   contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
