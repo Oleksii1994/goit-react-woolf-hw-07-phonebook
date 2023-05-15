177e765f9a012c96467e6d95e0f23b783968d413
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-05-16 02:04:40 +0300

    added createSelector for code optimization

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,28 +2,19 @@ import { ContactItemLi, DeleteBtn } from './ContactItem.styled';
 // import { deleteContactAction } from 'redux/contactsSlice';
 import { deleteContact } from 'redux/operations';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectContactsArr, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 import Notiflix from 'notiflix';
 
 export const ContactItem = () => {
   const dispatch = useDispatch();
-
-  const contactsState = useSelector(selectContactsArr);
-  const filterState = useSelector(selectFilter);
-
-  function getVisibleContacts() {
-    const normalizedFilter = filterState.toLowerCase();
-    return contactsState.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  }
+  const visibleContacts = useSelector(selectVisibleContacts);
 
   const handleDelete = (id, name) => {
     dispatch(deleteContact(id));
     Notiflix.Notify.success(`Contact ${name} deleted successfully`);
   };
 
-  return getVisibleContacts().map(({ id, name, phone }) => {
+  return visibleContacts.map(({ id, name, phone }) => {
     return (
       <ContactItemLi key={id}>
         {name}: {phone}
--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,6 +1,16 @@
-export const selectContacts = state => state.contacts;
+import { createSelector } from '@reduxjs/toolkit';
 
+export const selectContacts = state => state.contacts;
 export const selectContactsArr = state => state.contacts.contactsArr;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.contactsArr.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
